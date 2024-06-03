a35297c5bf659a26794beee1b856be559fc83280
theRealJqz <jiaqizhang01@gmail.com>
2024-06-03 17:12:57 -0400

    final fix???

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 async function router(){
     const rootName = "/hash-router-test"
     const routes = {
+        "/": {
+            title: "home page",
+            location: rootName + "/templates/home.html",
+            description: "This is the home page"
+        },
         "about": {
             title: "about page",
             location: rootName + "/templates/about.html",
@@ -8,9 +13,12 @@ async function router(){
         }
     };
     let endpoint = window.location.hash.replace("#", "");
+    if(endpoint.length === 0){
+        endpoint = "/"
+    }
     if(!routes[endpoint] && window.location.hash.length > 0){
         return window.location.assign(
-            "https://therealjqz.github.io/hash-router-test/404"
+            "https://therealjqz.github.io/hash-router-test/error"
           );          
     }
     const html = await fetch(routes[endpoint].location).then(data => data.text());
