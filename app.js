43105410861bae4cf8b2effffc8bd2ee43f4d7d2
theRealJqz <jiaqizhang01@gmail.com>
2024-06-04 16:12:29 -0400

    finished router

--- app.js
+++ app.js
@@ -1,33 +1,74 @@
-async function router(){
-    const rootName = "/hash-router-test"
-    const routes = {
-        "home": {
-            title: "home page",
-            location: rootName + "/templates/home.html",
-            description: "This is the home page"
-        },
-        "about": {
-            title: "about page",
-            location: rootName + "/templates/about.html",
-            description: "This is the about"
-        }
+function debounce(func, wait = 300, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
     };
+};
+// const rootName = "/hash-router-test"
+const rootName = ""
+const routes = {
+    "home": {
+        title: "home page",
+        location: rootName + "/templates/home.html",
+    },
+    "about": {
+        title: "about page",
+        location: rootName + "/templates/about.html",
+    },
+    "skills": {
+        title: "skills page",
+        location: rootName + "/templates/skills.html",
+    },
+    "projects": {
+        title: "projects page",
+        location: rootName + "/templates/projects.html",
+    },
+    "contact": {
+        title: "contact page",
+        location: rootName + "/templates/contact.html",
+    },
+};
+function handleNavBarDisplay(position){
+    const nav = document.querySelector("#navTop");
+    if(position > 2){
+        nav.classList.add("roaming");
+    }
+    else nav.classList.remove("roaming");
+}
+function handleTitle(location){
+    document.title = routes[location].title;
+}
+function handleScrollEvent(){//handle scrolls. Changes nav bar. load adjacent cards
+    handleNavBarDisplay(window.scrollY);
+}
+async function router(){
     let endpoint = window.location.hash.replace("#", "");
-    console.log(endpoint)
     if(endpoint.length === 0){
-        console.log("home page")
         endpoint = "home"
     }
     if(!routes[endpoint] && window.location.hash.length > 0){
         return window.location.assign(
-            "https://therealjqz.github.io/hash-router-test/error"
+            "https://therealjqz.github.io/hash-router-test/404.html"
           );          
     }
     const html = await fetch(routes[endpoint].location).then(data => data.text());
-    document.querySelector("#content").innerHTML = html;
-    document.title = routes[endpoint].title;
-    //if route is valid find corresponding file
+    //note only populate section when near
+    const card = document.querySelector(`#${endpoint}`);
+    card.innerHTML = html;
+    handleTitle(endpoint)
 };
 
 window.addEventListener("hashchange", router);
+document.addEventListener('scroll', ()=>{
+    debounce(handleScrollEvent())
+})
+
 router();
